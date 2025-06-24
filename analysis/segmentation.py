64e315ac1f6560040a79dc00b8f9edafdddea3e3
lovexruby <ruben.menc@hotmail.com>
2025-06-24 22:55:48 +0200

    Calculated clustersize with elbow method

--- analysis/segmentation.py
+++ analysis/segmentation.py
@@ -17,16 +17,36 @@ X = df[['Annual Income (k$)', 'Spending Score (1-100)']]
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# Initialize K-Means with 5 clusters
+"""
+# Use the Elbow Method to determine the optional number of clusters
+inertia = []
+k_range = range(1, 11)
+
+for k in k_range:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X_scaled)
+    inertia.append(kmeans.inertia_)
+"""
+
+
+
+# Initialize K-Means with calculated 5 clusters
 kmeans = KMeans(n_clusters=5, random_state=42)
-kmeans.fit(X_scaled)
+clusters = kmeans.fit_predict(X_scaled)
 
 # Assign cluster labels to original dataframe
-df['Cluster'] = kmeans.labels_
+df['Cluster'] = clusters
 
 # Visualize clusters
 plt.figure(figsize=(8,6))
-sns.scatterplot(x='Annual Income (k$)', y='Spending Score (1-100)', hue='Cluster', palette='Set1', data=df)
-plt.title('Customer Segments')
+scatter = plt.scatter(df['Annual Income (k$)'], df['Spending Score (1-100)'], c=df['Cluster'], cmap='viridis', s=50)
+#plt.plot(k_range, inertia, marker='o')
+#sns.scatterplot(x='Annual Income (k$)', y='Spending Score (1-100)', hue='Cluster', palette='Set1', data=df)
+plt.title("Customer Segments")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1-100)")
+plt.colorbar(scatter, label='Cluster')
+plt.grid(True)
+#plt.tight_layout
 plt.savefig('../plots/customer_segments.png')
 plt.show()
